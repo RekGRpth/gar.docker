a392f0f7b4e516493a9765b89145c087603ea751
RekGRpth <rekgrpth@gmail.com>
2021-09-04 08:53:07 +0500

    up

--- bin/xml2csv_all.sh
+++ bin/xml2csv_all.sh
@@ -4,5 +4,5 @@ find -type f -name "*.xsd" | sort -u | while read -r xsd; do
     filename="$(basename -- "${xsd}")"
     filename="${filename%.*}"
     table="$(echo "${filename}" | sed -E "s|_[[:digit:]]+||gi")"
-    find -type f -name "${table}*.XML" | sort -u | xargs -r -n 1 -P "$(nproc)" xml2csv_one.sh "${xsd}"
+    find -type f -name "${table}*_2.XML" | sort -u | xargs -r -n 1 -P "$(nproc)" xml2csv_one.sh "${xsd}"
 done
