5286885d3bf6ce2c0edffea18693ceeaaa3a254d
RekGRpth <rekgrpth@gmail.com>
2021-09-05 09:18:01 +0500

    up

--- bin/xml2csv_all.sh
+++ bin/xml2csv_all.sh
@@ -4,3 +4,6 @@ find /usr/local/xsd -type f -name "*.xsd" | sort -u | while read -r xsd; do
     xml="$(basename -- "${xsd%.*}")"
     find -type f -name "as_${xml}_2*.xml" | sort -u | xargs -r -n 1 -P "$(nproc)" xml2csv_one.sh "${xsd}"
 done
+xsd=/usr/local/xsd/param.xsd
+xml="$(basename -- "${xsd%.*}")"
+find -type f -name "as_*_${xml}s_2*.xml" | sort -u | xargs -r -n 1 -P "$(nproc)" xml2csv_one.sh "${xsd}"
