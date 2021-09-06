69b2eff3d8b92d81a0232483ecc1e0546fb480f1
RekGRpth <rekgrpth@gmail.com>
2021-09-06 13:58:31 +0500

    up

--- bin/csv2pg.sh
+++ bin/csv2pg.sh
@@ -6,5 +6,5 @@ XSD="/usr/local/xsd/$TABLE.xsd"
 DIR="$(dirname -- "$CSV")"
 DIR="$(basename -- "$DIR")"
 if echo "$DIR" | grep -E "^\d\d$" >/dev/null; then TABLE="${TABLE}_$DIR"; fi
-FIELDS="$(xmlstarlet select --text --noblanks --template --value-of /xs:schema/xs:element/xs:complexType/xs:sequence/xs:element/xs:complexType/xs:attribute/@name "$XSD" | sed 's/\(.*\)/\L\1/' | paste -sd ",")"
-cat "$CSV" | psql --no-password --variable=ON_ERROR_STOP=1 --command="COPY $TABLE ($FIELDS) FROM stdin WITH (FORMAT csv, DELIMITER ';')"
+FIELDS="$(xmlstarlet select --text --noblanks --template --value-of /xs:schema/xs:element/xs:complexType/xs:sequence/xs:element/xs:complexType/xs:attribute/@name "$XSD" | sed -uE 's|^(.+)$|"\1"|' | sed 's/\(.*\)/\L\1/' | paste -sd ",")"
+cat "$CSV" | psql --no-password --variable=ON_ERROR_STOP=1 --command="COPY \"$TABLE\" ($FIELDS) FROM stdin WITH (FORMAT csv, DELIMITER ';')"
