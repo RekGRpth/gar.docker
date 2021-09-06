f94af4dbcbccde7db60723490956b6ca34a13aca
RekGRpth <rekgrpth@gmail.com>
2021-09-06 16:06:12 +0500

    up

--- bin/csv2pg.sh
+++ bin/csv2pg.sh
@@ -7,9 +7,11 @@ DIR="$(dirname -- "$CSV")"
 DIR="$(basename -- "$DIR")"
 if echo "$DIR" | grep -P "^\d\d$" >/dev/null; then TABLE="${TABLE}_$DIR"; fi
 FIELDS="$(xmlstarlet select --text --noblanks --template --value-of /xs:schema/xs:element/xs:complexType/xs:sequence/xs:element/xs:complexType/xs:attribute/@name "$XSD" | sed -uE 's|^(.+)$|"\1"|' | sed 's/\(.*\)/\L\1/' | paste -sd ",")"
+FORCE_NOT_NULL="$(xmlstarlet select --text --noblanks --template --match /xs:schema/xs:element/xs:complexType/xs:sequence/xs:element/xs:complexType/xs:attribute --value-of @use --output ";" --value-of @name --nl "$XSD" | grep -v optional | sed -uE 's|^required;||' | sed -uE 's|^(.+)$|"\1"|' | sed 's/\(.*\)/\L\1/' | paste -sd ",")"
 deltaVersionId="$(cat .deltaVersionId)"
 fullVersionId="$(cat .fullVersionId)"
 if [ "$deltaVersionId" == "$fullVersionId" ]; then
     psql --no-password --variable=ON_ERROR_STOP=1 --command="TRUNCATE TABLE ONLY \"$TABLE\" RESTART IDENTITY CASCADE"
 fi
-cat "$CSV" | psql --no-password --variable=ON_ERROR_STOP=1 --command="COPY \"$TABLE\" ($FIELDS) FROM stdin WITH (FORMAT csv, DELIMITER E';', QUOTE E'\b')"
+cat "$CSV" | psql --no-password --variable=ON_ERROR_STOP=1 --command="COPY \"$TABLE\" ($FIELDS) FROM stdin WITH (FORMAT csv, DELIMITER E';', QUOTE E'\b', FORCE_NOT_NULL ($FORCE_NOT_NULL))"
+FORCE_NOT_NULL ()
