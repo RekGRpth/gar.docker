43c92102cf68f86fa255fd96c228c5a2a57512e2
RekGRpth <rekgrpth@gmail.com>
2021-09-06 09:33:54 +0500

    up

--- bin/csv2pg.sh
+++ bin/csv2pg.sh
@@ -5,5 +5,5 @@ CSV="$2"
 XSD="/usr/local/xsd/$TABLE.xsd"
 DIR="$(dirname -- "$CSV")"
 DIR="$(basename -- "$DIR")"
-if [ "$DIR" ~ [[:digit:]][[:digit:]] ]; then TABLE="%{TABLE}_$DIR"; fi
+if [ "$DIR" ~ [[:digit:]][[:digit:]] ]; then TABLE="${TABLE}_$DIR"; fi
 cat "$CSV" | psql -w -A -v ON_ERROR_STOP=1 -q -t -c "COPY $TABLE FROM stdin"
