047d761f17dce403ee09d7a2b733a133c818c80b
RekGRpth <rekgrpth@gmail.com>
2021-09-06 14:07:05 +0500

    truncate

--- bin/csv2pg.sh
+++ bin/csv2pg.sh
@@ -7,4 +7,9 @@ DIR="$(dirname -- "$CSV")"
 DIR="$(basename -- "$DIR")"
 if echo "$DIR" | grep -E "^\d\d$" >/dev/null; then TABLE="${TABLE}_$DIR"; fi
 FIELDS="$(xmlstarlet select --text --noblanks --template --value-of /xs:schema/xs:element/xs:complexType/xs:sequence/xs:element/xs:complexType/xs:attribute/@name "$XSD" | sed -uE 's|^(.+)$|"\1"|' | sed 's/\(.*\)/\L\1/' | paste -sd ",")"
+deltaVersionId="$(cat .deltaVersionId)"
+fullVersionId="$(cat .fullVersionId)"
+if [ "$deltaVersionId" == "$fullVersionId" ]; then
+    psql --no-password --variable=ON_ERROR_STOP=1 --command="TRUNCATE TABLE ONLY \"$TABLE\" RESTART IDENTITY CASCADE"
+fi
 cat "$CSV" | psql --no-password --variable=ON_ERROR_STOP=1 --command="COPY \"$TABLE\" ($FIELDS) FROM stdin WITH (FORMAT csv, DELIMITER ';')"
