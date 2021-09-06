6f5fe7f25d2a3ce208fa723795684e8997faafb4
RekGRpth <rekgrpth@gmail.com>
2021-09-06 16:10:05 +0500

    up

--- bin/cron.sh
+++ bin/cron.sh
@@ -9,13 +9,13 @@ while
         "csv2pg" )
             find /usr/local/xsd -type f -name "*.xsd" | sort -u | while read -r XSD; do
                 TABLE="$(basename -- "${XSD%.*}")"
-                find -type f -name "as_${TABLE}_2*.csv" | sort -u | xargs -r -n 1 -P "$(nproc)" csv2pg.sh "$TABLE" || exit 255
+                find . -type f -name "as_${TABLE}_2*.csv" | sort -u | xargs -r -n 1 -P "$(nproc)" csv2pg.sh "$TABLE" || exit 255
                 echo "$?"
             done
-            echo done >.state
+            echo "done" >.state
         ;;
         "unzip" )
-            ls *.zip | sort -u | while read -r ZIP; do
+            find . -type f -name "*.zip" | sort -u | while read -r ZIP; do
                 unzip -ouLL "$ZIP" -d "${ZIP%.*}"
                 rm -f "$ZIP"
             done
@@ -24,25 +24,25 @@ while
         "xml2csv" )
             find /usr/local/xsd -type f -name "*.xsd" | sort -u | while read -r XSD; do
                 TABLE="$(basename -- "${XSD%.*}")"
-                find -type f -name "as_${TABLE}_2*.xml" | sort -u | xargs -r -n 1 -P "$(nproc)" xml2csv.sh "$TABLE" || exit 255
+                find . -type f -name "as_${TABLE}_2*.xml" | sort -u | xargs -r -n 1 -P "$(nproc)" xml2csv.sh "$TABLE" || exit 255
                 echo "$?"
             done
             echo csv2pg >.state
         ;;
         * )
-            echo done >.state
+            echo "done" >.state
             deltaVersionId="$(cat .deltaVersionId)"
             if [ -z "$deltaVersionId" ]; then
                 wget --continue --output-document=.GetLastDownloadFileInfo https://fias.nalog.ru/WebServices/Public/GetLastDownloadFileInfo
-                lastVersionId="$(cat .GetLastDownloadFileInfo | jq --raw-output .VersionId)"
-                URL="$(cat .GetLastDownloadFileInfo | jq --raw-output .GarXMLFullURL)"
+                lastVersionId="$(jq --raw-output .VersionId <.GetLastDownloadFileInfo)"
+                URL="$(jq --raw-output .GarXMLFullURL <.GetLastDownloadFileInfo)"
                 ZIP="$lastVersionId.zip"
                 wget --continue --output-document="$ZIP" "$URL"
                 echo "$lastVersionId" >.deltaVersionId
                 echo "$lastVersionId" >.fullVersionId
             else
                 wget --continue --output-document=.GetAllDownloadFileInfo https://fias.nalog.ru/WebServices/Public/GetAllDownloadFileInfo
-                cat .GetAllDownloadFileInfo | jq --raw-output "sort_by(.VersionId) | .[] | select(.VersionId > $deltaVersionId) | [.VersionId, .GarXMLDeltaURL] | join(\";\")" | while IFS=';' read -r lastVersionId GarXMLDeltaURL; do
+                jq --raw-output "sort_by(.VersionId) | .[] | select(.VersionId > $deltaVersionId) | [.VersionId, .GarXMLDeltaURL] | join(\";\")" <.GetAllDownloadFileInfo | while IFS=';' read -r lastVersionId GarXMLDeltaURL; do
                     URL="$GarXMLDeltaURL"
                     ZIP="$lastVersionId.zip"
                     wget --continue --output-document="$ZIP" "$URL"
