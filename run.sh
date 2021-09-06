a8293d3c18b4655799401f72f1e8ec3fab90ed91
RekGRpth <rekgrpth@gmail.com>
2021-09-06 09:50:26 +0500

    up

---
+++ run.sh
@@ -0,0 +1,20 @@
+#!/bin/sh -eux
+
+docker pull ghcr.io/rekgrpth/gar.docker
+docker network create --attachable --opt com.docker.network.bridge.name=docker docker || echo $?
+docker stop gar || echo $?
+docker rm gar || echo $?
+docker run \
+    --env GROUP_ID="$(id -g)" \
+    --env LANG=ru_RU.UTF-8 \
+    --env TZ=Asia/Yekaterinburg \
+    --env USER_ID="$(id -u)" \
+    --hostname gar \
+    --interactive \
+    --mount type=bind,source=/etc/certs,destination=/etc/certs,readonly \
+    --mount type=bind,source=/home/georgy/data/load/gar,destination=/home \
+    --name gar \
+    --network name=docker \
+    --rm \
+    --tty \
+    ghcr.io/rekgrpth/postgres.docker sh
