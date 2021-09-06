2002596576cb4e4e7a244489282c70137980e9f9
RekGRpth <rekgrpth@gmail.com>
2021-09-06 13:20:32 +0500

    up

--- bin/csv2pg.sh
+++ bin/csv2pg.sh
@@ -5,5 +5,5 @@ CSV="$2"
 XSD="/usr/local/xsd/$TABLE.xsd"
 DIR="$(dirname -- "$CSV")"
 DIR="$(basename -- "$DIR")"
-if [ "$DIR" ~ [[:digit:]][[:digit:]] ]; then TABLE="${TABLE}_$DIR"; fi
+if echo "$DIR" | grep -E "^\d\d$" >/dev/null; then TABLE="${TABLE}_$DIR"; fi
 cat "$CSV" | psql --no-password --variable=ON_ERROR_STOP=1 --command="COPY $TABLE FROM stdin"
