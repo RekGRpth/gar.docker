2cb5ced0e8b12420194e7c1685afbb68f7e5547b
RekGRpth <rekgrpth@gmail.com>
2021-09-07 12:52:58 +0500

    up

--- bin/xml2csv.sh
+++ bin/xml2csv.sh
@@ -3,4 +3,4 @@
 RECORD="$1"
 FIELDS="$2"
 XML="$3"
-exec xml2 <"$XML" | tr -d "\t" | 2csv -d "	" "${RECORD}"${FIELDS} >"${XML%.*}.csv"
+exec xml2 <"$XML" | tr -d "\t" | 2csv -d "	" "$RECORD"$FIELDS >"${XML%.*}.csv"
