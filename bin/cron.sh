1fce9bd888aa8a9ad61a18d5fab40a33d927d340
RekGRpth <rekgrpth@gmail.com>
2021-09-07 14:12:28 +0500

    up

--- bin/cron.sh
+++ bin/cron.sh
@@ -24,7 +24,7 @@ while
         ;;
         "sql2pg" )
             find /usr/local/sql -type f -name "*.sql" | sort -u | while read -r SQL; do
-                psql --no-password --variable=ON_ERROR_STOP=on --file="$SQL"
+                psql --no-password --file="$SQL"
             done
             echo -n wget >fullVersionId.txt
             echo -n wget >state.txt
