2b35e220759950afc87a67545e2f0ab2e6bbcfb1
RekGRpth <rekgrpth@gmail.com>
2021-09-07 09:56:17 +0500

    up

--- bin/csv2pg.sh
+++ bin/csv2pg.sh
@@ -12,9 +12,13 @@ DIR="$(basename -- "$DIR")"
 if echo "$DIR" | grep -P "^\d\d$" >/dev/null; then TABLE="${TABLE}_$DIR"; fi
 if [ -n "$TRUNCATE" ]; then
     psql --no-password --variable=ON_ERROR_STOP=1 --command="TRUNCATE TABLE ONLY \"$TABLE\" RESTART IDENTITY CASCADE"
-fi
-exec psql --no-password --variable=ON_ERROR_STOP=1 <"$CSV" --command=<<EOF
+    exec psql --no-password --variable=ON_ERROR_STOP=1 <"$CSV" --command=<<EOF
+COPY "$TABLE" ($FIELDS) FROM stdin WITH (FORMAT csv, DELIMITER E'\t', QUOTE E'\b', FORCE_NOT_NULL ($FORCE_NOT_NULL));
+EOF
+else
+    exec psql --no-password --variable=ON_ERROR_STOP=1 <"$CSV" --command=<<EOF
 CREATE TEMP TABLE tmp (LIKE "$TABLE" INCLUDING ALL) ON COMMIT DROP;
 COPY tmp ($FIELDS) FROM stdin WITH (FORMAT csv, DELIMITER E'\t', QUOTE E'\b', FORCE_NOT_NULL ($FORCE_NOT_NULL));
 INSERT INTO "$TABLE" SELECT $FIELDS FROM tmp ON CONFLICT DO UPDATE SET $UPDATE;
 EOF
+fi
