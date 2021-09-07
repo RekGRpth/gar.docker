dfe6509180a655e5f0e5bdcd2d3436212734eabd
RekGRpth <rekgrpth@gmail.com>
2021-09-07 15:00:11 +0500

    up

--- bin/csv2pg.sh
+++ bin/csv2pg.sh
@@ -6,9 +6,12 @@ FIELDS="$3"
 FORCE_NOT_NULL="$4"
 UPDATE="$5"
 FULL="$6"
+DIR="$(dirname -- "$CSV")"
+DIR="$(basename -- "$DIR")"
+if echo "$DIR" | grep -P "^\d\d$" >/dev/null; then TABLE="${TABLE}_$DIR"; fi
 if [ -n "$FULL" ]; then
     COMMAND="COPY \"$TABLE\" ($FIELDS) FROM stdin WITH (FORMAT csv, DELIMITER E'\t', QUOTE E'\b', FORCE_NOT_NULL ($FORCE_NOT_NULL));"
 else
     COMMAND="CREATE TEMP TABLE tmp (LIKE \"$TABLE\" INCLUDING ALL) ON COMMIT DROP;COPY tmp ($FIELDS) FROM stdin WITH (FORMAT csv, DELIMITER E'\t', QUOTE E'\b', FORCE_NOT_NULL ($FORCE_NOT_NULL));INSERT INTO \"$TABLE\" SELECT $FIELDS FROM tmp ON CONFLICT DO UPDATE SET $UPDATE;"
 fi
-exec psql --no-password --command="$COMMAND" <"$CSV" || exit 255
+exec psql --no-password --variable=ON_ERROR_STOP=1 --command="$COMMAND" <"$CSV" || exit 255
