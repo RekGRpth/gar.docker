93bafa0842bd6b465498817f42885cef4ab759de
RekGRpth <rekgrpth@gmail.com>
2021-09-07 10:33:12 +0500

    up

--- bin/csv2pg.sh
+++ bin/csv2pg.sh
@@ -12,7 +12,7 @@ DIR="$(basename -- "$DIR")"
 if echo "$DIR" | grep -P "^\d\d$" >/dev/null; then TABLE="${TABLE}_$DIR"; fi
 if [ -n "$TRUNCATE" ]; then
     psql --no-password --variable=ON_ERROR_STOP=1 --command="TRUNCATE TABLE ONLY \"$TABLE\" RESTART IDENTITY CASCADE"
-    exec psql --no-password --variable=ON_ERROR_STOP=1 --command="COPY "$TABLE" ($FIELDS) FROM stdin WITH (FORMAT csv, DELIMITER E'\t', QUOTE E'\b', FORCE_NOT_NULL ($FORCE_NOT_NULL));" <"$CSV"
+    exec psql --no-password --variable=ON_ERROR_STOP=1 --command="COPY \"$TABLE\" ($FIELDS) FROM stdin WITH (FORMAT csv, DELIMITER E'\t', QUOTE E'\b', FORCE_NOT_NULL ($FORCE_NOT_NULL));" <"$CSV"
 else
-    exec psql --no-password --variable=ON_ERROR_STOP=1 --command="CREATE TEMP TABLE tmp (LIKE "$TABLE" INCLUDING ALL) ON COMMIT DROP;COPY tmp ($FIELDS) FROM stdin WITH (FORMAT csv, DELIMITER E'\t', QUOTE E'\b', FORCE_NOT_NULL ($FORCE_NOT_NULL));INSERT INTO "$TABLE" SELECT $FIELDS FROM tmp ON CONFLICT DO UPDATE SET $UPDATE;" <"$CSV"
+    exec psql --no-password --variable=ON_ERROR_STOP=1 --command="CREATE TEMP TABLE tmp (LIKE \"$TABLE\" INCLUDING ALL) ON COMMIT DROP;COPY tmp ($FIELDS) FROM stdin WITH (FORMAT csv, DELIMITER E'\t', QUOTE E'\b', FORCE_NOT_NULL ($FORCE_NOT_NULL));INSERT INTO \"$TABLE\" SELECT $FIELDS FROM tmp ON CONFLICT DO UPDATE SET $UPDATE;" <"$CSV"
 fi
