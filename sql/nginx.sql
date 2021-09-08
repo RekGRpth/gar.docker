be4f3f621fc46cd9889d0394ba6bd907a6f4b008
RekGRpth <rekgrpth@gmail.com>
2021-09-08 12:19:20 +0500

    up

--- sql/nginx.sql
+++ sql/nginx.sql
@@ -110,3 +110,21 @@ begin
         ) select to_json(_) from _ into strict gar_select.json;
     end if;
 end;$body$;
+CREATE OR REPLACE FUNCTION gar_update(INOUT json json) RETURNS json LANGUAGE plpgsql AS $body$ <<local>> declare
+    query json default gar_update.json->'json_get_vars'; -- параметры запроса
+    uuid uuid default nullif(trim(local.query->>'uuid'), '')::uuid; -- уид
+    parent_uuid uuid default nullif(trim(local.query->>'parent_uuid'), '')::uuid; -- уид родителя
+    name text default nullif(trim(local.query->>'name'), ''); -- наименование
+    short text default nullif(trim(local.query->>'short'), ''); -- кратко
+    type text default nullif(trim(local.query->>'type'), ''); -- тип
+    post text default nullif(trim(local.query->>'port'), ''); -- индекс
+    level integer default nullif(trim(local.query->>'level'), '')::integer; -- уровень
+    user text default gar_update.json->>'remote_user'; -- пользователь
+    text text default nullif(trim(local.query->>'text'), ''); -- текст
+begin
+    with _ as (
+        with _ as (
+            select * from gar_update(local.uuid, local.parent_uuid, local.name, local.short, local.type, local.post, local.level, local.user, local.text)
+        ) select local.query, to_json(_) as data from _
+    ) select to_json(_) from _ into strict gar_update.json;
+end;$body$;
