aa7c69b5fad8dddb8ec13b78218aaae24c48b8bb
RekGRpth <rekgrpth@gmail.com>
2021-09-09 08:25:52 +0500

    up

--- sql/object_levels.sql
+++ sql/object_levels.sql
@@ -1,11 +1,11 @@
 CREATE TABLE IF NOT EXISTS object_levels (
-  level INT NOT NULL PRIMARY KEY,
-  name VARCHAR(250) NOT NULL,
-  shortname VARCHAR(50),
-  updatedate DATE NOT NULL,
-  startdate DATE NOT NULL,
-  enddate DATE NOT NULL,
-  isactive BOOLEAN NOT NULL
+    level INT NOT NULL PRIMARY KEY,
+    name VARCHAR(250) NOT NULL,
+    shortname VARCHAR(50),
+    updatedate DATE NOT NULL,
+    startdate DATE NOT NULL,
+    enddate DATE NOT NULL,
+    isactive BOOLEAN NOT NULL
 );
 COMMENT ON TABLE object_levels IS 'Сведения по уровням адресных объектов';
 COMMENT ON COLUMN object_levels.level IS 'Уникальный идентификатор записи. Ключевое поле. Номер уровня объекта';
@@ -15,3 +15,7 @@ COMMENT ON COLUMN object_levels.updatedate IS 'Дата внесения (обн
 COMMENT ON COLUMN object_levels.startdate IS 'Начало действия записи';
 COMMENT ON COLUMN object_levels.enddate IS 'Окончание действия записи';
 COMMENT ON COLUMN object_levels.isactive IS 'Признак действующего адресного объекта';
+CREATE INDEX IF NOT EXISTS object_levels_shortname_idx ON object_levels USING btree (shortname);
+CREATE INDEX IF NOT EXISTS object_levels_startdate_idx ON object_levels USING btree (startdate);
+CREATE INDEX IF NOT EXISTS object_levels_enddate_idx ON object_levels USING btree (enddate);
+CREATE INDEX IF NOT EXISTS object_levels_isactive_idx ON object_levels USING btree (isactive);
--- sql/operation_types.sql
+++ sql/operation_types.sql
@@ -1,12 +1,12 @@
 CREATE TABLE IF NOT EXISTS operation_types (
-  id INT NOT NULL PRIMARY KEY,
-  name VARCHAR(100) NOT NULL,
-  shortname VARCHAR(100),
-  "desc" VARCHAR(250),
-  updatedate DATE NOT NULL,
-  startdate DATE NOT NULL,
-  enddate DATE NOT NULL,
-  isactive BOOLEAN NOT NULL
+    id INT NOT NULL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    shortname VARCHAR(100),
+    "desc" VARCHAR(250),
+    updatedate DATE NOT NULL,
+    startdate DATE NOT NULL,
+    enddate DATE NOT NULL,
+    isactive BOOLEAN NOT NULL
 );
 COMMENT ON TABLE operation_types IS 'Сведения по статусу действия';
 COMMENT ON COLUMN operation_types.id IS 'Идентификатор статуса (ключ)';
@@ -17,3 +17,6 @@ COMMENT ON COLUMN operation_types.updatedate IS 'Дата внесения (об
 COMMENT ON COLUMN operation_types.startdate IS 'Начало действия записи';
 COMMENT ON COLUMN operation_types.enddate IS 'Окончание действия записи';
 COMMENT ON COLUMN operation_types.isactive IS 'Статус активности';
+CREATE INDEX IF NOT EXISTS operation_types_startdate_idx ON operation_types USING btree (startdate);
+CREATE INDEX IF NOT EXISTS operation_types_enddate_idx ON operation_types USING btree (enddate);
+CREATE INDEX IF NOT EXISTS operation_types_isactive_idx ON operation_types USING btree (isactive);
