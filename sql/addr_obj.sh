312984c23e8498e72e911d8b3b54f42c8c268267
RekGRpth <rekgrpth@gmail.com>
2021-09-09 08:38:02 +0500

    up

--- sql/addr_obj.sh
+++ sql/addr_obj.sh
@@ -2,28 +2,29 @@
 
 DIR="$1"
 exec psql --no-password --variable=ON_ERROR_STOP=1 <<EOF
-CREATE TABLE IF NOT EXISTS addr_obj_$DIR (CONSTRAINT addr_obj_${DIR}_pkey PRIMARY KEY (id)) INHERITS (addr_obj);
-COMMENT ON TABLE addr_obj_$DIR IS 'Сведения классификатора адресообразующих элементов';
-COMMENT ON COLUMN addr_obj_$DIR.id IS 'Уникальный идентификатор записи. Ключевое поле';
-COMMENT ON COLUMN addr_obj_$DIR.objectid IS 'Глобальный уникальный идентификатор адресного объекта типа INTEGER';
-COMMENT ON COLUMN addr_obj_$DIR.objectguid IS 'Глобальный уникальный идентификатор адресного объекта типа UUID';
-COMMENT ON COLUMN addr_obj_$DIR.changeid IS 'ID изменившей транзакции';
-COMMENT ON COLUMN addr_obj_$DIR.name IS 'Наименование';
-COMMENT ON COLUMN addr_obj_$DIR.typename IS 'Краткое наименование типа объекта';
-COMMENT ON COLUMN addr_obj_$DIR.level IS 'Уровень адресного объекта';
-COMMENT ON COLUMN addr_obj_$DIR.opertypeid IS 'Статус действия над записью – причина появления записи';
-COMMENT ON COLUMN addr_obj_$DIR.previd IS 'Идентификатор записи связывания с предыдущей исторической записью';
-COMMENT ON COLUMN addr_obj_$DIR.nextid IS 'Идентификатор записи связывания с последующей исторической записью';
-COMMENT ON COLUMN addr_obj_$DIR.updatedate IS 'Дата внесения (обновления) записи';
-COMMENT ON COLUMN addr_obj_$DIR.startdate IS 'Начало действия записи';
-COMMENT ON COLUMN addr_obj_$DIR.enddate IS 'Окончание действия записи';
-COMMENT ON COLUMN addr_obj_$DIR.isactual IS 'Статус актуальности адресного объекта ФИАС';
-COMMENT ON COLUMN addr_obj_$DIR.isactive IS 'Признак действующего адресного объекта';
-CREATE INDEX IF NOT EXISTS addr_obj_${DIR}_objectid_idx ON addr_obj_$DIR USING btree (objectid);
-CREATE INDEX IF NOT EXISTS addr_obj_${DIR}_typename_idx ON addr_obj_$DIR USING btree (typename);
-CREATE INDEX IF NOT EXISTS addr_obj_${DIR}_level_idx ON addr_obj_$DIR USING btree (level);
-CREATE INDEX IF NOT EXISTS addr_obj_${DIR}_startdate_idx ON addr_obj_$DIR USING btree (startdate);
-CREATE INDEX IF NOT EXISTS addr_obj_${DIR}_enddate_idx ON addr_obj_$DIR USING btree (enddate);
-CREATE INDEX IF NOT EXISTS addr_obj_${DIR}_isactual_idx ON addr_obj_$DIR USING btree (isactual);
-CREATE INDEX IF NOT EXISTS addr_obj_${DIR}_isactive_idx ON addr_obj_$DIR USING btree (isactive);
+CREATE SCHEMA IF NOT EXISTS $DIR;
+CREATE TABLE IF NOT EXISTS $DIR.addr_obj (CONSTRAINT $DIR.addr_obj_pkey PRIMARY KEY (id)) INHERITS (addr_obj);
+COMMENT ON TABLE $DIR.addr_obj IS 'Сведения классификатора адресообразующих элементов';
+COMMENT ON COLUMN $DIR.addr_obj.id IS 'Уникальный идентификатор записи. Ключевое поле';
+COMMENT ON COLUMN $DIR.addr_obj.objectid IS 'Глобальный уникальный идентификатор адресного объекта типа INTEGER';
+COMMENT ON COLUMN $DIR.addr_obj.objectguid IS 'Глобальный уникальный идентификатор адресного объекта типа UUID';
+COMMENT ON COLUMN $DIR.addr_obj.changeid IS 'ID изменившей транзакции';
+COMMENT ON COLUMN $DIR.addr_obj.name IS 'Наименование';
+COMMENT ON COLUMN $DIR.addr_obj.typename IS 'Краткое наименование типа объекта';
+COMMENT ON COLUMN $DIR.addr_obj.level IS 'Уровень адресного объекта';
+COMMENT ON COLUMN $DIR.addr_obj.opertypeid IS 'Статус действия над записью – причина появления записи';
+COMMENT ON COLUMN $DIR.addr_obj.previd IS 'Идентификатор записи связывания с предыдущей исторической записью';
+COMMENT ON COLUMN $DIR.addr_obj.nextid IS 'Идентификатор записи связывания с последующей исторической записью';
+COMMENT ON COLUMN $DIR.addr_obj.updatedate IS 'Дата внесения (обновления) записи';
+COMMENT ON COLUMN $DIR.addr_obj.startdate IS 'Начало действия записи';
+COMMENT ON COLUMN $DIR.addr_obj.enddate IS 'Окончание действия записи';
+COMMENT ON COLUMN $DIR.addr_obj.isactual IS 'Статус актуальности адресного объекта ФИАС';
+COMMENT ON COLUMN $DIR.addr_obj.isactive IS 'Признак действующего адресного объекта';
+CREATE INDEX IF NOT EXISTS $DIR.addr_obj_objectid_idx ON $DIR.addr_obj USING btree (objectid);
+CREATE INDEX IF NOT EXISTS $DIR.addr_obj_typename_idx ON $DIR.addr_obj USING btree (typename);
+CREATE INDEX IF NOT EXISTS $DIR.addr_obj_level_idx ON $DIR.addr_obj USING btree (level);
+CREATE INDEX IF NOT EXISTS $DIR.addr_obj_startdate_idx ON $DIR.addr_obj USING btree (startdate);
+CREATE INDEX IF NOT EXISTS $DIR.addr_obj_enddate_idx ON $DIR.addr_obj USING btree (enddate);
+CREATE INDEX IF NOT EXISTS $DIR.addr_obj_isactual_idx ON $DIR.addr_obj USING btree (isactual);
+CREATE INDEX IF NOT EXISTS $DIR.addr_obj_isactive_idx ON $DIR.addr_obj USING btree (isactive);
 EOF
