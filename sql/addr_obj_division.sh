7697a65731d9e69320eadb73a35c61ab0b2328ea
RekGRpth <rekgrpth@gmail.com>
2021-09-09 08:36:54 +0500

    up

--- sql/addr_obj_division.sh
+++ sql/addr_obj_division.sh
@@ -2,10 +2,11 @@
 
 DIR="$1"
 exec psql --no-password --variable=ON_ERROR_STOP=1 <<EOF
-CREATE TABLE IF NOT EXISTS addr_obj_division_$DIR (CONSTRAINT addr_obj_division_${DIR}_pkey PRIMARY KEY (id)) INHERITS (addr_obj_division);
-COMMENT ON TABLE addr_obj_division_$DIR IS 'Сведения по операциям переподчинения';
-COMMENT ON COLUMN addr_obj_division_$DIR.id IS 'Уникальный идентификатор записи. Ключевое поле';
-COMMENT ON COLUMN addr_obj_division_$DIR.parentid IS 'Родительский ID';
-COMMENT ON COLUMN addr_obj_division_$DIR.childid IS 'Дочерний ID';
-COMMENT ON COLUMN addr_obj_division_$DIR.changeid IS 'ID изменившей транзакции';
+CREATE SCHEMA IF NOT EXISTS $DIR;
+CREATE TABLE IF NOT EXISTS $DIR.addr_obj_division (CONSTRAINT $DIR.addr_obj_division_pkey PRIMARY KEY (id)) INHERITS (addr_obj_division);
+COMMENT ON TABLE $DIR.addr_obj_division IS 'Сведения по операциям переподчинения';
+COMMENT ON COLUMN $DIR.addr_obj_division.id IS 'Уникальный идентификатор записи. Ключевое поле';
+COMMENT ON COLUMN $DIR.addr_obj_division.parentid IS 'Родительский ID';
+COMMENT ON COLUMN $DIR.addr_obj_division.childid IS 'Дочерний ID';
+COMMENT ON COLUMN $DIR.addr_obj_division.changeid IS 'ID изменившей транзакции';
 EOF
