6cfa35da2fed4d884535c916d36ab4d1121375c7
RekGRpth <rekgrpth@gmail.com>
2021-09-10 09:16:36 +0500

    rename

--- sql/gar.sql
+++ sql/gar.sql
@@ -1,34 +1,34 @@
 CREATE TABLE IF NOT EXISTS gar (
-    uuid uuid NOT NULL DEFAULT gen_random_uuid(),
+    id uuid NOT NULL DEFAULT gen_random_uuid(),
     parent uuid,
     name text NOT NULL,
     short text NOT NULL,
     type text NOT NULL,
     post text,
-    CONSTRAINT gar_pkey PRIMARY KEY (uuid),
+    CONSTRAINT gar_pkey PRIMARY KEY (id),
     CONSTRAINT gar_name_short_type_key UNIQUE (parent, name, type)
 );
-CREATE OR REPLACE FUNCTION gar_child(uuid uuid) RETURNS bigint LANGUAGE sql STABLE AS $body$
-    select count(1) from gar where parent = gar_child.uuid;
+CREATE OR REPLACE FUNCTION gar_child(id uuid) RETURNS bigint LANGUAGE sql STABLE AS $body$
+    select count(1) from gar where parent = gar_child.id;
 $body$;
-CREATE OR REPLACE FUNCTION gar_insert(uuid uuid, parent uuid, name text, short text, type text, post text) RETURNS gar LANGUAGE sql AS $body$
-    insert into gar (uuid, parent, name, short, type, post)
-    values (coalesce(gar_insert.uuid, gen_random_uuid()), gar_insert.parent, gar_insert.name, gar_insert.short, gar_insert.type, gar_insert.post) returning *;
+CREATE OR REPLACE FUNCTION gar_insert(id uuid, parent uuid, name text, short text, type text, post text) RETURNS gar LANGUAGE sql AS $body$
+    insert into gar (id, parent, name, short, type, post)
+    values (coalesce(gar_insert.id, gen_random_uuid()), gar_insert.parent, gar_insert.name, gar_insert.short, gar_insert.type, gar_insert.post) returning *;
 $body$;
-CREATE OR REPLACE FUNCTION gar_select(uuid uuid[]) RETURNS SETOF gar LANGUAGE sql STABLE AS $body$
+CREATE OR REPLACE FUNCTION gar_select(id uuid[]) RETURNS SETOF gar LANGUAGE sql STABLE AS $body$
     select gar.* from gar
-    inner join (select unnest(gar_select.uuid) as uuid, generate_subscripts(gar_select.uuid, 1) as i) as _ on _.uuid = gar.uuid
-    where gar.uuid = any(gar_select.uuid) order by i;
+    inner join (select unnest(gar_select.id) as id, generate_subscripts(gar_select.id, 1) as i) as _ on _.id = gar.id
+    where gar.id = any(gar_select.id) order by i;
 $body$;
-CREATE OR REPLACE FUNCTION gar_select(uuid uuid, parent uuid) RETURNS SETOF gar LANGUAGE sql STABLE AS $body$
+CREATE OR REPLACE FUNCTION gar_select(id uuid, parent uuid) RETURNS SETOF gar LANGUAGE sql STABLE AS $body$
     with recursive _ as (
-        select gar.*, 0 as i from gar where uuid = gar_select.uuid
+        select gar.*, 0 as i from gar where id = gar_select.id
         union
-        select gar.*, _.i + 1 as i from gar inner join _ on (_.parent = gar.uuid)
+        select gar.*, _.i + 1 as i from gar inner join _ on (_.parent = gar.id)
         where gar_select.parent is null or _.parent != gar_select.parent
-    ) select uuid, parent, name, short, type, post from _ order by i desc;
+    ) select id, parent, name, short, type, post from _ order by i desc;
 $body$;
-CREATE OR REPLACE FUNCTION gar_select(parent uuid, name text, short text, type text, post text) RETURNS SETOF gar LANGUAGE sql STABLE AS $body$
+CREATE OR REPLACE FUNCTION gar_select(parent id, name text, short text, type text, post text) RETURNS SETOF gar LANGUAGE sql STABLE AS $body$
     select * from gar where true
     and ((gar_select.parent is null and parent is null) or parent = gar_select.parent)
     and (gar_select.name is null or name ilike gar_select.name||'%' or name ilike '% '||gar_select.name||'%' or name ilike '%-'||gar_select.name||'%' or name ilike '%.'||gar_select.name||'%')
@@ -40,43 +40,43 @@ $body$;
 CREATE OR REPLACE FUNCTION gar_text(name text, short text, type text) RETURNS text LANGUAGE sql IMMUTABLE AS $body$
     select case when gar_text.type in ('Местность') then gar_text.name when gar_text.name ilike '%'||gar_text.type||'%' then gar_text.name else gar_text.short||'.'||gar_text.name end;
 $body$;
-CREATE OR REPLACE FUNCTION gar_text(uuid uuid[], post boolean DEFAULT NULL, "all" boolean DEFAULT NULL) RETURNS text LANGUAGE sql STABLE AS $body$
+CREATE OR REPLACE FUNCTION gar_text(id uuid[], post boolean DEFAULT NULL, "all" boolean DEFAULT NULL) RETURNS text LANGUAGE sql STABLE AS $body$
     with _ as (
         with _ as (
-            select * from gar_select(gar_text.uuid)
+            select * from gar_select(gar_text.id)
         ), p as (
             select post from _ where coalesce(gar_text.post, false) and post is not null limit 1
         ) select post as text from p union select string_agg(gar_text(name, short, type), ', ') as text from _ where type not in ('Подъезд', 'Этаж') or coalesce(gar_text.all, false)
     ) select string_agg(text, ', ') from _
 $body$;
-CREATE OR REPLACE FUNCTION gar_text(uuid uuid, post boolean DEFAULT NULL, "all" boolean DEFAULT NULL) RETURNS text LANGUAGE sql STABLE AS $body$
+CREATE OR REPLACE FUNCTION gar_text(id uuid, post boolean DEFAULT NULL, "all" boolean DEFAULT NULL) RETURNS text LANGUAGE sql STABLE AS $body$
     with _ as (
         with _ as (
-            select * from gar_select(gar_text.uuid, null)
+            select * from gar_select(gar_text.id, null)
         ), p as (
             select post from _ where coalesce(gar_text.post, false) and post is not null limit 1
         ) select post as text from p union select string_agg(gar_text(name, short, type), ', ') as text from _ where type not in ('Подъезд', 'Этаж') or coalesce(gar_text.all, false)
     ) select string_agg(text, ', ') from _
 $body$;
-CREATE OR REPLACE FUNCTION gar_full(uuid uuid) RETURNS text LANGUAGE sql STABLE AS $body$
-    select gar_text(gar_full.uuid, true, true);
+CREATE OR REPLACE FUNCTION gar_full(id uuid) RETURNS text LANGUAGE sql STABLE AS $body$
+    select gar_text(gar_full.id, true, true);
 $body$;
 CREATE OR REPLACE FUNCTION gar_trigger() RETURNS trigger LANGUAGE plpgsql AS $body$ <<local>> declare
 BEGIN
     if TG_OP in ('INSERT', 'UPDATE') then RETURN new; elsif TG_OP = 'DELETE' then RETURN old; end if;
 END;$body$;
-CREATE OR REPLACE FUNCTION gar_update(uuid uuid, parent uuid, name text, short text, type text, post text) RETURNS gar LANGUAGE sql AS $body$
+CREATE OR REPLACE FUNCTION gar_update(id uuid, parent uuid, name text, short text, type text, post text) RETURNS gar LANGUAGE sql AS $body$
     UPDATE gar SET
         parent = coalesce(gar_update.parent, parent),
         name = coalesce(gar_update.name, name),
         short = coalesce(gar_update.short, short),
         type = coalesce(gar_update.type, type),
         post = coalesce(gar_update.post, post)
-    WHERE uuid = gar_update.uuid returning *;
+    WHERE id = gar_update.id returning *;
 $body$;
-CREATE OR REPLACE FUNCTION gar_uuid(uuid uuid) RETURNS uuid[] LANGUAGE sql STABLE AS $body$
-    select array_agg(uuid) from gar_select(gar_uuid.uuid, null);
-$body$;
+/*CREATE OR REPLACE FUNCTION gar_id(id uuid) RETURNS uuid[] LANGUAGE sql STABLE AS $body$
+    select array_agg(id) from gar_select(gar_id.id, null);
+$body$;*/
 CREATE INDEX IF NOT EXISTS gar_parent_idx ON gar USING btree (parent);
 CREATE INDEX IF NOT EXISTS gar_name_idx ON gar USING btree (name);
 CREATE INDEX IF NOT EXISTS gar_short_idx ON gar USING btree (short);
--- sql/nginx.sql
+++ sql/nginx.sql
@@ -2,7 +2,7 @@ CREATE SCHEMA IF NOT EXISTS nginx AUTHORIZATION nginx;
 \connect gar nginx
 CREATE OR REPLACE FUNCTION gar_insert(INOUT json json) RETURNS json LANGUAGE plpgsql AS $body$ <<local>> declare
     query json default gar_insert.json->'json_get_vars'; -- параметры запроса
-    uuid uuid default nullif(trim(local.query->>'uuid'), '')::uuid; -- уид
+    id uuid default nullif(trim(local.query->>'id'), '')::uuid; -- уид
     parent uuid default nullif(trim(local.query->>'parent'), '')::uuid; -- уид родителя
     name text default nullif(trim(local.query->>'name'), ''); -- наименование
     short text default nullif(trim(local.query->>'short'), ''); -- кратко
@@ -11,13 +11,13 @@ CREATE OR REPLACE FUNCTION gar_insert(INOUT json json) RETURNS json LANGUAGE plp
 begin
     with _ as (
         with _ as (
-            select * from gar_insert(local.uuid, local.parent, local.name, local.short, local.type, local.post)
+            select * from gar_insert(local.id, local.parent, local.name, local.short, local.type, local.post)
         ) select local.query, to_json(_) as data from _
     ) select to_json(_) from _ into strict gar_insert.json;
 end;$body$;
 CREATE OR REPLACE FUNCTION gar_select(INOUT json json) RETURNS json LANGUAGE plpgsql STABLE AS $body$ <<local>> declare
     query json default gar_select.json->'json_get_vars'; -- параметры запроса
-    uuid uuid default nullif(trim(local.query->>'uuid'), '')::uuid; -- уид
+    id uuid default nullif(trim(local.query->>'id'), '')::uuid; -- уид
     parent uuid default nullif(trim(local.query->>'parent'), '')::uuid; -- уид родителя
     name text default nullif(trim(local.query->>'name'), ''); -- наименование
     short text default nullif(trim(local.query->>'short'), ''); -- кратко
@@ -29,56 +29,56 @@ CREATE OR REPLACE FUNCTION gar_select(INOUT json json) RETURNS json LANGUAGE plp
     "all" boolean default coalesce(nullif(trim(local.query->>'all'), '')::boolean, false); -- все?
     child boolean default coalesce(nullif(trim(local.query->>'child'), '')::boolean, false); -- дети?
 begin
-    if local.uuid is not null then -- если задан uuid
-        local.uuid = translate(local.uuid, '[]','{}');
-        if local.uuid ilike '{%}' then -- если uuid - массив
+    if local.id is not null then -- если задан id
+        local.id = translate(local.id, '[]','{}');
+        if local.id ilike '{%}' then -- если id - массив
             if "all" then -- если все результаты
                 with _ as (
                     with _ as (
-                        select * from gar_select(local.uuid::uuid[])
+                        select * from gar_select(local.id::uuid[])
                     ) select count(1), local.query, local.offset, local.limit, (
                         with _ as (
                             select * from _ offset local.offset limit local.limit
                         ) select coalesce(json_agg((select json_agg(_) from (
-                            select *, case when local.child then gar_child(uuid) end as child from gar_select(_.uuid::uuid)
+                            select *, case when local.child then gar_child(id) end as child from gar_select(_.id::uuid)
                         ) as _)), '[]'::json) from _
                     ) as data from _
                 ) select to_json(_) from _ into strict gar_select.json;
             else -- иначе - не все результаты
                 with _ as (
                     with _ as (
-                        select * from gar_select(local.uuid::uuid[])
+                        select * from gar_select(local.id::uuid[])
                     ) select count(1), local.query, local.offset, local.limit, (
                         with _ as (
-                            select *, case when local.child then gar_child(uuid) end as child from _ offset local.offset limit local.limit
+                            select *, case when local.child then gar_child(id) end as child from _ offset local.offset limit local.limit
                         ) select coalesce(json_agg(_), '[]'::json) from _
                     ) as data from _
                 ) select to_json(_) from _ into strict gar_select.json;
             end if;
-        else  -- иначе uuid - не массив
+        else  -- иначе id - не массив
             if "all" then -- если все результаты
                 with _ as (
                     with _ as (
-                        select * from gar_select(local.uuid::uuid, null)
+                        select * from gar_select(local.id::uuid, null)
                     ) select count(1), local.query, local.offset, local.limit, (
                         with _ as (
-                            select *, case when local.child then gar_child(uuid) end as child from _ offset local.offset limit local.limit
+                            select *, case when local.child then gar_child(id) end as child from _ offset local.offset limit local.limit
                         ) select coalesce(json_agg(_), '[]'::json) from _
                     ) as data from _
                 ) select to_json(_) from _ into strict gar_select.json;
             else -- иначе - не все результаты
                 with _ as (
                     with _ as (
-                        select * from gar_select(local.uuid::uuid)
+                        select * from gar_select(local.id::uuid)
                     ) select count(1), local.query, local.offset, local.limit, (
                         with _ as (
-                            select *, case when local.child then gar_child(uuid) end as child from _ offset local.offset limit local.limit
+                            select *, case when local.child then gar_child(id) end as child from _ offset local.offset limit local.limit
                         ) select coalesce(json_agg(_), '[]'::json) from _
                     ) as data from _
                 ) select to_json(_) from _ into strict gar_select.json;
             end if;
         end if;
-    else -- иначе - не задан uuid
+    else -- иначе - не задан id
         if local.term is not null then -- если искать что-то
             local.name = term;
             local.short = split_part(local.name, '.', 1);
@@ -95,7 +95,7 @@ begin
                 select * from gar_select(local.parent, local.name, local.short, local.type, local.post)
             ) select count(1), local.query, local.offset, local.limit, (
                 with _ as (
-                    select *, case when local.child then gar_child(uuid) end as child from _ offset local.offset limit local.limit
+                    select *, case when local.child then gar_child(id) end as child from _ offset local.offset limit local.limit
                 ) select coalesce(json_agg(_), '[]'::json) from _
             ) as data from _
         ) select to_json(_) from _ into strict gar_select.json;
@@ -103,7 +103,7 @@ begin
 end;$body$;
 CREATE OR REPLACE FUNCTION gar_update(INOUT json json) RETURNS json LANGUAGE plpgsql AS $body$ <<local>> declare
     query json default gar_update.json->'json_get_vars'; -- параметры запроса
-    uuid uuid default nullif(trim(local.query->>'uuid'), '')::uuid; -- уид
+    id uuid default nullif(trim(local.query->>'id'), '')::uuid; -- уид
     parent uuid default nullif(trim(local.query->>'parent'), '')::uuid; -- уид родителя
     name text default nullif(trim(local.query->>'name'), ''); -- наименование
     short text default nullif(trim(local.query->>'short'), ''); -- кратко
@@ -112,7 +112,7 @@ CREATE OR REPLACE FUNCTION gar_update(INOUT json json) RETURNS json LANGUAGE plp
 begin
     with _ as (
         with _ as (
-            select * from gar_update(local.uuid, local.parent, local.name, local.short, local.type, local.post)
+            select * from gar_update(local.id, local.parent, local.name, local.short, local.type, local.post)
         ) select local.query, to_json(_) as data from _
     ) select to_json(_) from _ into strict gar_update.json;
 end;$body$;
