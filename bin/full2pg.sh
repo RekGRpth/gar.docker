ddfa72d475de232347881abd843b6db8426e0639
RekGRpth <rekgrpth@gmail.com>
2021-09-10 08:50:32 +0500

    truncate

--- bin/full2pg.sh
+++ bin/full2pg.sh
@@ -7,5 +7,5 @@ FORCE_NOT_NULL="$4"
 DIR="$(dirname -- "$CSV")"
 DIR="$(basename -- "$DIR")"
 if echo "$DIR" | grep -P "^\d\d$" >/dev/null; then TABLE="\"${DIR}\".$TABLE"; fi
-COMMAND="COPY $TABLE ($FIELDS) FROM stdin WITH (FORMAT csv, DELIMITER E'\t', QUOTE E'\b', FORCE_NOT_NULL ($FORCE_NOT_NULL));"
+COMMAND="TRUNCATE TABLE ONLY $TABLE RESTART IDENTITY;COPY $TABLE ($FIELDS) FROM stdin WITH (FORMAT csv, DELIMITER E'\t', QUOTE E'\b', FORCE_NOT_NULL ($FORCE_NOT_NULL));"
 exec psql --no-password --variable=ON_ERROR_STOP=1 --command="$COMMAND" <"$CSV"
