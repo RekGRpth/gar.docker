65b52efd1e05ded5e63dd042216eef5dabdd4169
RekGRpth <rekgrpth@gmail.com>
2021-09-10 11:19:05 +0500

    up

--- sql/adm_hierarchy.sh
+++ sql/adm_hierarchy.sh
@@ -43,6 +43,21 @@ BEGIN
     left join "${DIR}".addr_obj_params as addr_obj_params on addr_obj_params.objectid = addr_obj.objectid and addr_obj_params.typeid = param_types.id and current_timestamp between addr_obj_params.startdate and addr_obj_params.enddate
     WHERE addr_obj.isactive = 1 and addr_obj.isactual = 1 and current_timestamp between addr_obj.startdate and addr_obj.enddate and addr_obj.objectid = new.objectid
     on conflict (id) do update set parent = EXCLUDED.parent, name = EXCLUDED.name, short = EXCLUDED.short, type = EXCLUDED.type, post = EXCLUDED.post;
+    insert into gar
+    SELECT distinct on (parent, name, type)
+        houses.objectguid AS id,
+        houses_parent.objectguid AS parent,
+        houses.housenum AS name,
+        rtrim(house_types.shortname, '.') AS short,
+        house_types.name AS type,
+        houses_params.value AS post
+    FROM "${DIR}".houses as houses
+    inner JOIN house_types ON house_types.id = houses.housetype and house_types.isactive --and current_timestamp between house_types.startdate and house_types.enddate
+    left join "${DIR}".addr_obj as houses_parent on houses_parent.objectid = new.parentobjid and houses_parent.isactive = 1 and houses_parent.isactual = 1 and current_timestamp between houses_parent.startdate and houses_parent.enddate
+    left join param_types on param_types.name = 'Почтовый индекс' and param_types.isactive and current_timestamp between param_types.startdate and param_types.enddate
+    left join "${DIR}".houses_params as houses_params on houses_params.objectid = houses.objectid and houses_params.typeid = param_types.id and current_timestamp between houses_params.startdate and houses_params.enddate
+    WHERE houses.isactive = 1 and houses.isactual = 1 and current_timestamp between houses.startdate and houses.enddate
+    on conflict (id) do update set parent = EXCLUDED.parent, name = EXCLUDED.name, short = EXCLUDED.short, type = EXCLUDED.type, post = EXCLUDED.post;
     return new;
 END;$body$;
 CREATE TRIGGER adm_hierarchy_after_trigger AFTER INSERT OR UPDATE ON "${DIR}".adm_hierarchy FOR EACH ROW WHEN (now() between new.startdate and new.enddate and new.isactive = 1) EXECUTE PROCEDURE "${DIR}".adm_hierarchy_trigger();
