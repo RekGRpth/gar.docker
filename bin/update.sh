065fdee01faf7b9d0585455deec3742a938ff807
RekGRpth <rekgrpth@gmail.com>
2021-09-13 08:58:47 +0500

    mv

--- bin/update.sh
+++ bin/update.sh
@@ -44,23 +44,6 @@ EOF
 psql --no-password --variable=ON_ERROR_STOP=1 <<EOF
 with _ as (
     SELECT
-        carplaces.objectguid AS id,
-        carplaces_parent.objectguid AS parent,
-        carplaces.number AS name,
-        'м-м' AS short,
-        'Машино-место' AS type,
-        carplaces_params.value AS post
-    FROM "${DIR}".carplaces as carplaces
-    left join "${DIR}".adm_hierarchy as adm_hierarchy on adm_hierarchy.objectid = carplaces.objectid and adm_hierarchy.isactive = 1 and current_timestamp between adm_hierarchy.startdate and adm_hierarchy.enddate
-    left join "${DIR}".houses as carplaces_parent on carplaces_parent.objectid = adm_hierarchy.parentobjid and carplaces_parent.isactive = 1 and carplaces_parent.isactual = 1 and current_timestamp between carplaces_parent.startdate and carplaces_parent.enddate
-    left join param_types on param_types.name = 'Почтовый индекс' and param_types.isactive and current_timestamp between param_types.startdate and param_types.enddate
-    left join "${DIR}".carplaces_params as carplaces_params on carplaces_params.objectid = carplaces.objectid and carplaces_params.typeid = param_types.id and current_timestamp between carplaces_params.startdate and carplaces_params.enddate
-    WHERE carplaces.isactive = 1 and carplaces.isactual = 1 and current_timestamp between carplaces.startdate and carplaces.enddate
-) insert into gar SELECT distinct on (parent, name, type) * from _ WHERE parent is not null on conflict (id) do update set parent = EXCLUDED.parent, name = EXCLUDED.name, short = EXCLUDED.short, type = EXCLUDED.type, post = EXCLUDED.post
-EOF
-psql --no-password --variable=ON_ERROR_STOP=1 <<EOF
-with _ as (
-    SELECT
         houses.objectguid AS id,
         houses_parent.objectguid AS parent,
         concat_ws(', ', houses.housenum, case when houses.addnum1 is not null then concat(case when coalesce(house_types1.id, houses.housetype) = houses.housetype then 'корп' else rtrim(house_types1.shortname, '.') end, '.', houses.addnum1) end, case when houses.addnum2 is not null then concat(case when coalesce(house_types2.id, houses.housetype) = houses.housetype then 'стр' else rtrim(house_types2.shortname, '.') end, '.', houses.addnum2) end) AS name,
@@ -79,6 +62,23 @@ with _ as (
     WHERE houses.isactive = 1 and houses.isactual = 1 and current_timestamp between houses.startdate and houses.enddate
     and gar.id is null
 ) insert into gar SELECT distinct on (parent, name, type) * from _ WHERE parent is not null on conflict (id) do update set parent = EXCLUDED.parent, name = EXCLUDED.name, short = EXCLUDED.short, type = EXCLUDED.type, post = EXCLUDED.post;
+EOF
+psql --no-password --variable=ON_ERROR_STOP=1 <<EOF
+with _ as (
+    SELECT
+        carplaces.objectguid AS id,
+        carplaces_parent.objectguid AS parent,
+        carplaces.number AS name,
+        'м-м' AS short,
+        'Машино-место' AS type,
+        carplaces_params.value AS post
+    FROM "${DIR}".carplaces as carplaces
+    left join "${DIR}".adm_hierarchy as adm_hierarchy on adm_hierarchy.objectid = carplaces.objectid and adm_hierarchy.isactive = 1 and current_timestamp between adm_hierarchy.startdate and adm_hierarchy.enddate
+    left join "${DIR}".houses as carplaces_parent on carplaces_parent.objectid = adm_hierarchy.parentobjid and carplaces_parent.isactive = 1 and carplaces_parent.isactual = 1 and current_timestamp between carplaces_parent.startdate and carplaces_parent.enddate
+    left join param_types on param_types.name = 'Почтовый индекс' and param_types.isactive and current_timestamp between param_types.startdate and param_types.enddate
+    left join "${DIR}".carplaces_params as carplaces_params on carplaces_params.objectid = carplaces.objectid and carplaces_params.typeid = param_types.id and current_timestamp between carplaces_params.startdate and carplaces_params.enddate
+    WHERE carplaces.isactive = 1 and carplaces.isactual = 1 and current_timestamp between carplaces.startdate and carplaces.enddate
+) insert into gar SELECT distinct on (parent, name, type) * from _ WHERE parent is not null on conflict (id) do update set parent = EXCLUDED.parent, name = EXCLUDED.name, short = EXCLUDED.short, type = EXCLUDED.type, post = EXCLUDED.post
 EOF
 psql --no-password --variable=ON_ERROR_STOP=1 <<EOF
 with _ as (
