1393af1393429ea3ba18768d0633a7e5fb5925ba
RekGRpth <rekgrpth@gmail.com>
2021-09-13 10:18:01 +0500

    up

--- sql/gar.sql
+++ sql/gar.sql
@@ -54,7 +54,7 @@ CREATE OR REPLACE FUNCTION gar_text(id uuid, post boolean DEFAULT NULL, "all" bo
         with _ as (
             select * from gar_select(gar_text.id, null)
         ), p as (
-            select post from _ where coalesce(gar_text.post, false) and post is not null limit 1
+            select unnest(array_agg(post)) as post, generate_subscripts(array_agg(post), 1) as i from _ where coalesce(true, false) and post is not null order by i desc limit 1
         ) select post as text from p union select string_agg(gar_text(name, short, type), ', ') as text from _ where type not in ('Подъезд', 'Этаж') or coalesce(gar_text.all, false)
     ) select string_agg(text, ', ') from _
 $body$;
