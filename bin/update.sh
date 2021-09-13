8b3e0eb51274ab5cdab8d1a1f0d875f501d2502b
RekGRpth <rekgrpth@gmail.com>
2021-09-13 08:53:59 +0500

    apartments

--- bin/update.sh
+++ bin/update.sh
@@ -98,3 +98,21 @@ with _ as (
     WHERE rooms.isactive = 1 and rooms.isactual = 1 and current_timestamp between rooms.startdate and rooms.enddate
 ) insert into gar SELECT distinct on (parent, name, type) * from _ WHERE parent is not null on conflict (id) do update set parent = EXCLUDED.parent, name = EXCLUDED.name, short = EXCLUDED.short, type = EXCLUDED.type, post = EXCLUDED.post
 EOF
+psql --no-password --variable=ON_ERROR_STOP=1 <<EOF
+with _ as (
+    SELECT
+        apartments.objectguid AS id,
+        apartments_parent.objectguid AS parent,
+        apartments.number AS name,
+        rtrim(apartment_types.shortname, '.') AS short,
+        apartment_types.name AS type,
+        apartments_params.value AS post
+    FROM "${DIR}".apartments as apartments
+    inner JOIN apartment_types ON apartment_types.id = apartments.aparttype and apartment_types.isactive --and current_timestamp between apartment_types.startdate and apartment_types.enddate
+    left join "${DIR}".adm_hierarchy as adm_hierarchy on adm_hierarchy.objectid = apartments.objectid and adm_hierarchy.isactive = 1 and current_timestamp between adm_hierarchy.startdate and adm_hierarchy.enddate
+    left join "${DIR}".houses as apartments_parent on apartments_parent.objectid = adm_hierarchy.parentobjid and apartments_parent.isactive = 1 and apartments_parent.isactual = 1 and current_timestamp between apartments_parent.startdate and apartments_parent.enddate
+    left join param_types on param_types.name = 'Почтовый индекс' and param_types.isactive and current_timestamp between param_types.startdate and param_types.enddate
+    left join "${DIR}".apartments_params as apartments_params on apartments_params.objectid = apartments.objectid and apartments_params.typeid = param_types.id and current_timestamp between apartments_params.startdate and apartments_params.enddate
+    WHERE apartments.isactive = 1 and apartments.isactual = 1 and current_timestamp between apartments.startdate and apartments.enddate
+) insert into gar SELECT distinct on (parent, name, type) * from _ WHERE parent is not null on conflict (id) do update set parent = EXCLUDED.parent, name = EXCLUDED.name, short = EXCLUDED.short, type = EXCLUDED.type, post = EXCLUDED.post
+EOF
