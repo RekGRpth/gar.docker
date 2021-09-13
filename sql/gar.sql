4cce737c34c05eab2ef115a5a78d33b38f1e31d5
RekGRpth <rekgrpth@gmail.com>
2021-09-13 11:31:38 +0500

    up

--- sql/gar.sql
+++ sql/gar.sql
@@ -25,9 +25,9 @@ CREATE TABLE IF NOT EXISTS gar (
 CREATE OR REPLACE FUNCTION gar_child(id uuid) RETURNS bigint LANGUAGE sql STABLE AS $body$
     select count(1) from gar where parent = gar_child.id;
 $body$;
-CREATE OR REPLACE FUNCTION gar_insert(id uuid, parent uuid, name text, short text, type text, post text) RETURNS gar LANGUAGE sql AS $body$
-    insert into gar (id, parent, name, short, type, post)
-    values (coalesce(gar_insert.id, gen_random_uuid()), gar_insert.parent, gar_insert.name, gar_insert.short, gar_insert.type, gar_insert.post) returning *;
+CREATE OR REPLACE FUNCTION gar_insert(id uuid, parent uuid, name text, short text, type text, post text, object object, region smallint) RETURNS gar LANGUAGE sql AS $body$
+    insert into gar (id, parent, name, short, type, post, object, region)
+    values (coalesce(gar_insert.id, gen_random_uuid()), gar_insert.parent, gar_insert.name, gar_insert.short, gar_insert.type, gar_insert.post, gar_insert.object, gar_insert.region) returning *;
 $body$;
 CREATE OR REPLACE FUNCTION gar_select(id uuid[]) RETURNS SETOF gar LANGUAGE sql STABLE AS $body$
     select gar.* from gar
@@ -42,13 +42,15 @@ CREATE OR REPLACE FUNCTION gar_select(id uuid, parent uuid) RETURNS SETOF gar LA
         where gar_select.parent is null or _.parent != gar_select.parent
     ) select id, parent, name, short, type, post, object, region from _ order by i desc;
 $body$;
-CREATE OR REPLACE FUNCTION gar_select(parent uuid, name text, short text, type text, post text) RETURNS SETOF gar LANGUAGE sql STABLE AS $body$
+CREATE OR REPLACE FUNCTION gar_select(parent uuid, name text, short text, type text, post text, object object, region smallint) RETURNS SETOF gar LANGUAGE sql STABLE AS $body$
     select * from gar where true
     and ((gar_select.parent is null and parent is null) or parent = gar_select.parent)
     and (gar_select.name is null or name ilike gar_select.name||'%' or name ilike '% '||gar_select.name||'%' or name ilike '%-'||gar_select.name||'%' or name ilike '%.'||gar_select.name||'%')
     and (gar_select.short is null or short ilike gar_select.short)
     and (gar_select.type is null or case when gar_select.type ilike '{%}' then type = any(gar_select.type::text[]) else type ilike gar_select.type||'%' end)
     and (gar_select.post is null or post ilike gar_select.post||'%')
+    and (gar_select.object is null or case when gar_select.object ilike '{%}' then object = any(gar_select.object::object[]) else object = gar_select.object end)
+    and (gar_select.region is null or case when gar_select.region ilike '{%}' then region = any(gar_select.region::smallint[]) else region = gar_select.region end)
     order by to_number('0'||name, '999999999'), name;
 $body$;
 CREATE OR REPLACE FUNCTION gar_text(name text, short text, type text) RETURNS text LANGUAGE sql IMMUTABLE AS $body$
@@ -79,13 +81,15 @@ $body$;
 BEGIN
     if TG_OP in ('INSERT', 'UPDATE') then RETURN new; elsif TG_OP = 'DELETE' then RETURN old; end if;
 END;$body$;*/
-CREATE OR REPLACE FUNCTION gar_update(id uuid, parent uuid, name text, short text, type text, post text) RETURNS gar LANGUAGE sql AS $body$
+CREATE OR REPLACE FUNCTION gar_update(id uuid, parent uuid, name text, short text, type text, post text, object object, region smallint) RETURNS gar LANGUAGE sql AS $body$
     UPDATE gar SET
         parent = coalesce(gar_update.parent, parent),
         name = coalesce(gar_update.name, name),
         short = coalesce(gar_update.short, short),
         type = coalesce(gar_update.type, type),
-        post = coalesce(gar_update.post, post)
+        post = coalesce(gar_update.post, post),
+        object = coalesce(gar_update.object, object),
+        region = coalesce(gar_update.region, region)
     WHERE id = gar_update.id returning *;
 $body$;
 /*CREATE OR REPLACE FUNCTION gar_id(id uuid) RETURNS uuid[] LANGUAGE sql STABLE AS $body$
