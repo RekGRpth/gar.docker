a2cda69f940f0ba0407db0a8909738e40bc1894c
RekGRpth <rekgrpth@gmail.com>
2021-09-13 08:50:55 +0500

    rooms

--- bin/update.sh
+++ bin/update.sh
@@ -80,3 +80,21 @@ with _ as (
     and gar.id is null
 ) insert into gar SELECT distinct on (parent, name, type) * from _ WHERE parent is not null on conflict (id) do update set parent = EXCLUDED.parent, name = EXCLUDED.name, short = EXCLUDED.short, type = EXCLUDED.type, post = EXCLUDED.post;
 EOF
+psql --no-password --variable=ON_ERROR_STOP=1 <<EOF
+with _ as (
+    SELECT
+        rooms.objectguid AS id,
+        rooms_parent.objectguid AS parent,
+        rooms.number AS name,
+        rtrim(room_types.shortname, '.') AS short,
+        room_types.name AS type,
+        rooms_params.value AS post
+    FROM "${DIR}".rooms as rooms
+    inner JOIN room_types ON room_types.id = rooms.roomtype and room_types.isactive --and current_timestamp between room_types.startdate and room_types.enddate
+    left join "${DIR}".adm_hierarchy as adm_hierarchy on adm_hierarchy.objectid = rooms.objectid and adm_hierarchy.isactive = 1 and current_timestamp between adm_hierarchy.startdate and adm_hierarchy.enddate
+    left join "${DIR}".houses as rooms_parent on rooms_parent.objectid = adm_hierarchy.parentobjid and rooms_parent.isactive = 1 and rooms_parent.isactual = 1 and current_timestamp between rooms_parent.startdate and rooms_parent.enddate
+    left join param_types on param_types.name = 'Почтовый индекс' and param_types.isactive and current_timestamp between param_types.startdate and param_types.enddate
+    left join "${DIR}".rooms_params as rooms_params on rooms_params.objectid = rooms.objectid and rooms_params.typeid = param_types.id and current_timestamp between rooms_params.startdate and rooms_params.enddate
+    WHERE rooms.isactive = 1 and rooms.isactual = 1 and current_timestamp between rooms.startdate and rooms.enddate
+) insert into gar SELECT distinct on (parent, name, type) * from _ WHERE parent is not null on conflict (id) do update set parent = EXCLUDED.parent, name = EXCLUDED.name, short = EXCLUDED.short, type = EXCLUDED.type, post = EXCLUDED.post
+EOF
