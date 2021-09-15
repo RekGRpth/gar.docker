0177e8ef387217b6f458cb67324f852acb819d90
RekGRpth <rekgrpth@gmail.com>
2021-09-15 13:19:41 +0500

    fix

--- bin/delta2pg.sh
+++ bin/delta2pg.sh
@@ -12,5 +12,5 @@ DIR="$(dirname -- "$CSV")"
 DIR="$(basename -- "$DIR")"
 if echo "$DIR" | grep -P "^\d\d$" >/dev/null; then TABLE="\"${DIR}\".$TABLE"; fi
 COMMAND="CREATE TEMP TABLE tmp (LIKE $TABLE INCLUDING ALL) ON COMMIT DROP;COPY tmp ($FIELDS) FROM stdin WITH (FORMAT csv, DELIMITER E'\t', QUOTE E'\b', FORCE_NOT_NULL ($FORCE_NOT_NULL));INSERT INTO $TABLE SELECT $FIELDS FROM tmp ON CONFLICT ($PRIMARY) DO UPDATE SET $UPDATE;"
-if [ -n "$ISACTIVE" ]; then COMMAND="DELETE FROM $TABLE WHERE NOT isactive" fi
+if [ -n "$ISACTIVE" ]; then COMMAND="DELETE FROM $TABLE WHERE NOT isactive"; fi
 exec psql --no-password --variable=ON_ERROR_STOP=1 --command="$COMMAND" <"$CSV"
