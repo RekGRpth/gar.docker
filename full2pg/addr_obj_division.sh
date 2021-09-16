b7636456b649095a4487fd9e3404ab6199f4850a
RekGRpth <rekgrpth@gmail.com>
2021-09-16 15:47:15 +0500

    full2pg

---
+++ full2pg/addr_obj_division.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -eux
+CSV="$1"
+TABLE="${0%.*}"
+DIR="$(dirname -- "$CSV")"
+DIR="$(basename -- "$DIR")"
+if echo "$DIR" | grep -P "^\d\d$" >/dev/null; then TABLE="\"$DIR\".\"$TABLE\""; fi
+COMMAND="$(cat <<EOF
+TRUNCATE TABLE ONLY $TABLE RESTART IDENTITY;
+COPY $TABLE ("id", "parentid", "childid", "changeid")
+FROM stdin WITH (FORMAT csv, DELIMITER E'\t', QUOTE E'\b', FORCE_NOT_NULL ("id", "parentid", "childid", "changeid"));
+EOF
+)"
+exec psql --no-password --variable=ON_ERROR_STOP=1 --command="$COMMAND" <"$CSV"
---
+++ full2pg/adm_hierarchy.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -eux
+CSV="$1"
+TABLE="${0%.*}"
+DIR="$(dirname -- "$CSV")"
+DIR="$(basename -- "$DIR")"
+if echo "$DIR" | grep -P "^\d\d$" >/dev/null; then TABLE="\"$DIR\".\"$TABLE\""; fi
+COMMAND="$(cat <<EOF
+TRUNCATE TABLE ONLY $TABLE RESTART IDENTITY;
+COPY $TABLE ("id", "objectid", "parentobjid", "changeid", "regioncode", "areacode", "citycode", "placecode", "plancode", "streetcode", "previd", "nextid", "updatedate", "startdate", "enddate", "isactive")
+FROM stdin WITH (FORMAT csv, DELIMITER E'\t', QUOTE E'\b', FORCE_NOT_NULL ("id", "objectid", "changeid", "updatedate", "startdate", "enddate", "isactive"))
+WHERE isactive::bool;'
+EOF
+)"
+exec psql --no-password --variable=ON_ERROR_STOP=1 --command="$COMMAND" <"$CSV"
