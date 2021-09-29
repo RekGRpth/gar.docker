e91a3c112369341746c567c082a4e3760a23b8f7
RekGRpth <rekgrpth@gmail.com>
2021-09-29 10:06:49 +0500

    fix

--- update/addr_obj.sh
+++ update/addr_obj.sh
@@ -20,7 +20,7 @@ with _ as (
     left join "${DIR}".addr_obj as addr_obj_parent on addr_obj_parent.objectid = adm_hierarchy.parentobjid
     left join param_types on param_types.name = 'Почтовый индекс'
     left join "${DIR}".addr_obj_params as addr_obj_params on addr_obj_params.objectid = addr_obj.objectid and addr_obj_params.typeid = param_types.id
-    left join gar on gar.object = 'addr_obj' and gar.region = ${DIR} and gar.parent = addr_obj_parent.objectguid and gar.name = addr_obj.name and gar.type = addr_obj_types.name
+    left join "${DIR}".gar as gar on gar.object = 'addr_obj' and gar.parent = addr_obj_parent.objectguid and gar.name = addr_obj.name and gar.type = addr_obj_types.name
     WHERE addr_obj.level = 1 and gar.id is null
 ) insert into gar SELECT distinct on (parent, name, type) * from _ on conflict (id) do update set
 parent = EXCLUDED.parent, name = EXCLUDED.name, short = EXCLUDED.short, type = EXCLUDED.type, post = EXCLUDED.post, object = EXCLUDED.object, region = EXCLUDED.region;
@@ -42,7 +42,7 @@ with _ as (
     left join "${DIR}".addr_obj as addr_obj_parent on addr_obj_parent.objectid = adm_hierarchy.parentobjid
     left join param_types on param_types.name = 'Почтовый индекс'
     left join "${DIR}".addr_obj_params as addr_obj_params on addr_obj_params.objectid = addr_obj.objectid and addr_obj_params.typeid = param_types.id
-    left join gar on gar.object = 'addr_obj' and gar.region = ${DIR} and gar.parent = addr_obj_parent.objectguid and gar.name = addr_obj.name and gar.type = addr_obj_types.name
+    left join "${DIR}".gar as gar on gar.object = 'addr_obj' and gar.parent = addr_obj_parent.objectguid and gar.name = addr_obj.name and gar.type = addr_obj_types.name
     WHERE gar.id is null
 ) insert into gar SELECT distinct on (parent, name, type) * from _ WHERE parent is not null on conflict (id) do update set
 parent = EXCLUDED.parent, name = EXCLUDED.name, short = EXCLUDED.short, type = EXCLUDED.type, post = EXCLUDED.post, object = EXCLUDED.object, region = EXCLUDED.region;
--- update/steads.sh
+++ update/steads.sh
@@ -19,7 +19,7 @@ with _ as (
     left join "${DIR}".addr_obj as steads_parent on steads_parent.objectid = adm_hierarchy.parentobjid
     left join param_types on param_types.name = 'Почтовый индекс'
     left join "${DIR}".steads_params as steads_params on steads_params.objectid = steads.objectid and steads_params.typeid = param_types.id
-    left join gar on gar.object = 'steads' and gar.region = ${DIR} and gar.parent = steads_parent.objectguid and gar.name = steads.number and gar.type = 'Участок'
+    left join "${DIR}".gar as gar on gar.object = 'steads' and gar.parent = steads_parent.objectguid and gar.name = steads.number and gar.type = 'Участок'
     WHERE gar.id is null
 ) insert into gar SELECT distinct on (parent, name, type) * from _ WHERE parent is not null on conflict (id) do update set
 parent = EXCLUDED.parent, name = EXCLUDED.name, short = EXCLUDED.short, type = EXCLUDED.type, post = EXCLUDED.post, object = EXCLUDED.object, region = EXCLUDED.region;
