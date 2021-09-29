96553a86b351695f5c0900106bdfb4d864e18fe9
RekGRpth <rekgrpth@gmail.com>
2021-09-29 11:20:37 +0500

    -schema

--- sql2pg/apartments_params.sh
+++ sql2pg/apartments_params.sh
@@ -4,6 +4,5 @@ set -eux
 trap "exit 255" ERR
 DIR="$1"
 psql --no-password --variable=ON_ERROR_STOP=1 <<EOF
-CREATE SCHEMA IF NOT EXISTS "${DIR}";
 CREATE TABLE IF NOT EXISTS "${DIR}".apartments_params PARTITION OF apartments_params FOR VALUES IN (${DIR});
 EOF
