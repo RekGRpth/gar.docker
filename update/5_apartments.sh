1d9cb971659feebe32adb41d31a9a154104e77b0
RekGRpth <rekgrpth@gmail.com>
2021-10-03 13:59:06 +0500

    op

--- update/5_apartments.sh
+++ update/5_apartments.sh
@@ -6,18 +6,19 @@ DIR="$1"
 psql --no-password --variable=ON_ERROR_STOP=1 <<EOF
 CREATE TEMPORARY TABLE s ON COMMIT DROP as
 SELECT
-    carplaces.objectguid AS id,
-    carplaces_parent.objectguid AS parent,
-    carplaces.number AS name,
-    'м-м' AS short,
-    'Машино-место' AS type,
-    carplaces_params.value AS post
-FROM "${DIR}".carplaces as carplaces
-left join "${DIR}".adm_hierarchy as adm_hierarchy on adm_hierarchy.objectid = carplaces.objectid
-left join "${DIR}".houses as carplaces_parent on carplaces_parent.objectid = adm_hierarchy.parentobjid
+    apartments.objectguid AS id,
+    apartments_parent.objectguid AS parent,
+    apartments.number AS name,
+    rtrim(apartment_types.shortname, '.') AS short,
+    apartment_types.name AS type,
+    apartments_params.value AS post
+FROM "${DIR}".apartments as apartments
+inner JOIN apartment_types ON apartment_types.id = apartments.aparttype
+left join "${DIR}".adm_hierarchy as adm_hierarchy on adm_hierarchy.objectid = apartments.objectid
+left join "${DIR}".houses as apartments_parent on apartments_parent.objectid = adm_hierarchy.parentobjid
 left join param_types on param_types.name = 'Почтовый индекс'
-left join "${DIR}".carplaces_params as carplaces_params on carplaces_params.objectid = carplaces.objectid and carplaces_params.typeid = param_types.id
-WHERE carplaces_parent.objectguid is not null;
+left join "${DIR}".apartments_params as apartments_params on apartments_params.objectid = apartments.objectid and apartments_params.typeid = param_types.id
+WHERE apartments_parent.objectguid is not null;
 CREATE TEMPORARY TABLE u ON COMMIT DROP as select s.* from s inner join gar as g using (id) where (s.parent, s.name, s.short, s.type, s.post) is distinct from (g.parent, g.name, g.short, g.type, g.post);
 CREATE TEMPORARY TABLE i ON COMMIT DROP as select s.* from s left join gar as g using (id) where g.id is null;
 with u as (
