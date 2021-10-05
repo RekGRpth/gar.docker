b0f1a63ac06dfc2fb875537dbbd34c37c3bb32ba
RekGRpth <rekgrpth@gmail.com>
2021-10-05 10:00:35 +0500

    op

--- update/6_rooms.sh
+++ update/6_rooms.sh
@@ -21,9 +21,9 @@ WITH s AS (
     LEFT JOIN "$REGION".rooms_params AS v ON v.objectid = o.objectid AND v.typeid = 5
     WHERE p.objectguid IS NOT NULL
 ), o AS (
-    SELECT s.* FROM s INNER JOIN gar AS g USING (id) WHERE g.object = 'rooms' AND g.region = $REGION AND (s.parent, s.name, s.short, s.type, s.post) IS DISTINCT FROM (g.parent, g.name, g.short, g.type, g.post) FOR UPDATE OF g SKIP LOCKED
+    SELECT g.id FROM s INNER JOIN gar AS g ON g.id = s.id AND g.object = 'addr_obj' AND g.region = $REGION AND (s.parent, s.name, s.short, s.type, s.post) IS DISTINCT FROM (g.parent, g.name, g.short, g.type, g.post) FOR UPDATE OF g SKIP LOCKED
 ), i AS (
-    INSERT INTO gar SELECT s.* FROM s LEFT JOIN gar AS g USING (id) WHERE g.id IS NULL RETURNING *
+    INSERT INTO gar SELECT s.* FROM s LEFT JOIN gar AS g ON g.id = s.id WHERE g.id IS NULL RETURNING *
 ), u AS (
     UPDATE gar AS g SET parent = o.parent, name = o.name, short = o.short, type = o.type FROM o WHERE g.id = o.id RETURNING *
 ) SELECT $REGION AS region, count(i.*) AS inserted, count(u.*) AS updated FROM i, u;
