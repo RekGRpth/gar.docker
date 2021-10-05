4654e06c674402eff04e00dfd98260305ffbc0bb
RekGRpth <rekgrpth@gmail.com>
2021-10-05 10:23:48 +0500

    up

--- update/3_houses.sh
+++ update/3_houses.sh
@@ -25,8 +25,8 @@ WITH s AS (
 ), o AS (
     SELECT s.* FROM s INNER JOIN gar AS g ON g.id = s.id AND g.object = 'addr_obj' AND g.region = $REGION AND (s.parent, s.name, s.short, s.type, s.post) IS DISTINCT FROM (g.parent, g.name, g.short, g.type, g.post) FOR UPDATE OF g SKIP LOCKED
 ), i AS (
-    INSERT INTO gar SELECT s.* FROM s LEFT JOIN gar AS g ON g.id = s.id WHERE g.id IS NULL RETURNING *
+    INSERT INTO gar AS g SELECT s.* FROM s LEFT JOIN gar AS g ON g.id = s.id WHERE g.id IS NULL RETURNING g.*, 'insert' as command
 ), u AS (
-    UPDATE gar AS g SET parent = o.parent, name = o.name, short = o.short, type = o.type FROM o WHERE g.id = o.id RETURNING *
-) SELECT $REGION AS region, count(i.*) AS inserted, count(u.*) AS updated FROM i, u;
+    UPDATE gar AS g SET parent = o.parent, name = o.name, short = o.short, type = o.type FROM o WHERE g.id = o.id RETURNING g.*, 'update' as command
+) SELECT $REGION AS region, command, count(*) FROM u GROUP BY command UNION SELECT $REGION AS region, command, count(*) FROM i GROUP BY command;
 EOF
