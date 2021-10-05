b4a261d9735425ce4517e6821f83bf76b67ed6d5
RekGRpth <rekgrpth@gmail.com>
2021-10-05 11:01:31 +0500

    op

--- update/6_rooms.sh
+++ update/6_rooms.sh
@@ -21,9 +21,9 @@ WITH s AS (
     LEFT JOIN "$REGION".rooms_params AS v ON v.objectid = o.objectid AND v.typeid = 5
     WHERE p.objectguid IS NOT NULL
 ), o AS (
-    SELECT s.* FROM s INNER JOIN gar AS g ON g.id = s.id AND g.object = 'rooms' AND g.region = $REGION WHERE (s.parent, s.name, s.short, s.type, s.post) IS DISTINCT FROM (g.parent, g.name, g.short, g.type, g.post) FOR UPDATE OF g SKIP LOCKED
+    SELECT s.* FROM s INNER JOIN gar AS g ON g.id = s.id AND g.object = s.object AND g.region = s.region WHERE (s.parent, s.name, s.short, s.type, s.post) IS DISTINCT FROM (g.parent, g.name, g.short, g.type, g.post) FOR UPDATE OF g SKIP LOCKED
 ), i AS (
-    INSERT INTO gar AS g SELECT s.* FROM s LEFT JOIN gar AS g ON g.id = s.id AND g.object = 'rooms' AND g.region = $REGION WHERE g.id IS NULL RETURNING g.*, 'insert' as command
+    INSERT INTO gar AS g SELECT s.* FROM s LEFT JOIN gar AS g ON g.id = s.id AND g.object = s.object AND g.region = s.region WHERE g.id IS NULL RETURNING g.*, 'insert' as command
 ), u AS (
     UPDATE gar AS g SET parent = o.parent, name = o.name, short = o.short, type = o.type FROM o WHERE g.id = o.id RETURNING g.*, 'update' as command
 ) SELECT $REGION AS region, command, count(*) FROM u GROUP BY command UNION SELECT $REGION AS region, command, count(*) FROM i GROUP BY command;
