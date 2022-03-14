baebbb6fbc861befa13019693aeb1ef9d6c8f809
RekGRpth <rekgrpth@gmail.com>
2022-03-14 08:31:57 +0500

    fix

--- local/bin/wget.sh
+++ local/bin/wget.sh
@@ -3,7 +3,7 @@
 set -eux
 trap "exit 255" ERR
 URL="$1"
-SIZE="$(curl -Is "$URL" | grep 'Content-Length' | grep -oP '\d+')"
+SIZE="$(curl -Is "$URL" | sed "s/\(.*\)/\L\1/" | grep 'content-length' | grep -oP '\d+')"
 test "$SIZE" -lt 1073741824
 ZIP="$(dirname -- "$URL")"
 ZIP="$(basename -- "$ZIP" | tr -d '.')"
